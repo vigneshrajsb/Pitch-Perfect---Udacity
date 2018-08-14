bbd442ff36d0210ab4c8cbdd0edf34103f9f6d80
Vigneshraj Sekar Babu <vigneshrajsekarbabu@vigneshrajs-macbook-pro.local>
2018-08-14 00:11:48 -0500

    complete

---
+++ Pitch Perfect/RecordingViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Vigneshraj Sekar Babu on 8/8/18.
+//  Copyright © 2018 Vigneshraj Sekar Babu. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordingViewController: UIViewController, AVAudioRecorderDelegate {
+    let segueStopRecord = "stopRecording"
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    var audioRecorder : AVAudioRecorder!
+    
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stopButton.isEnabled = false
+    }
+
+ 
+    //MARK: - Playback control
+    
+    @IBAction func recordPressed(_ sender: UIButton) {
+        configureUIFor(recordingFlag: true)
+        let directory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let fileName = "recording.wav"
+        let pathArray = [directory, fileName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, with: AVAudioSessionCategoryOptions.defaultToSpeaker)
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+    }
+    
+    @IBAction func stopPressed(_ sender: UIButton) {
+        configureUIFor(recordingFlag: false)
+        stopRecording()
+    }
+    
+    func stopRecording() {
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    //MARK: - AVAudioRecorderDelegate methods
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag {
+        performSegue(withIdentifier: segueStopRecord, sender: audioRecorder.url)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Recording Failed!", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Okay", style: .default) { (action) in
+                self.stopRecording()
+                self.configureUIFor(recordingFlag: false)
+            }
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    //MARK: - Segue methods
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueStopRecord {
+            if let destinationVC = segue.destination as? SoundViewController {
+                destinationVC.recordedAudioURL = sender as? URL
+            }
+        }
+    }
+    
+    func configureUIFor(recordingFlag: Bool) {
+        recordingLabel.text = recordingFlag ? "Recording" : "Recording Stopped"
+        stopButton.isEnabled = recordingFlag
+        recordButton.isEnabled = !recordingFlag
+    }
+}
+
---
+++ Pitch Perfect/SoundViewController.swift
@@ -0,0 +1,68 @@
+//
+//  SoundViewController.swift
+//  Pitch Perfect
+//
+//  Created by Vigneshraj Sekar Babu on 8/9/18.
+//  Copyright © 2018 Vigneshraj Sekar Babu. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundViewController: UIViewController {
+
+    var recordedAudioURL : URL!
+    
+    @IBOutlet weak var slowButton : UIButton!
+    @IBOutlet weak var fastButton : UIButton!
+    @IBOutlet weak var lowPitchButton : UIButton!
+    @IBOutlet weak var hightPitchButton : UIButton!
+    @IBOutlet weak var echoButton : UIButton!
+    @IBOutlet weak var reverbButton : UIButton!
+    @IBOutlet weak var stopButton : UIButton!
+    
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, vader, chipmunk, echo, reverb
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //print(recordedAudioURL.absoluteString)
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
+    }
+    
+    
+    //MARK: - Playback Control
+    
+    @IBAction func stopButtonPressed(_ sender: UIButton){
+        stopAudio()
+    }
+
+    @IBAction func playSoundForButton(_ sender: AnyObject) {
+        switch (ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .vader:
+            playSound(pitch: -1000)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.playing)
+    }
+    
+}
